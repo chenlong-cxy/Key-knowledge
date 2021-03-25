6558eca509b710b2fa6f905b0a3916d280828074
chenlong_cxy <chenlonghyl@163.com>
2021-03-25 14:07:41 +0800

    编译原理测试

---
+++ Principle_of_code_execution/Principle_of_code_execution/test.c
@@ -0,0 +1,12 @@
+#include <stdio.h>//“printf”未定义
+
+extern int Add(int, int);//“Add”未定义，链接时每个源文件单独编译
+
+int main()
+{
+	int a = 10;
+	int b = 20;
+	int c = Add(a, b);
+	printf("%d\n", c);
+	return 0;
+}
