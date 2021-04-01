95e068d482963af63cc1699f416582b544119d8c
chenlong_cxy <chenlonghyl@163.com>
2021-04-01 18:43:45 +0800

    增加

--- SeqList/SeqList/SeqList.c
+++ SeqList/SeqList/SeqList.c
@@ -14,7 +14,7 @@ void SeqListDestory(SeqList* ps)
 {
 	assert(ps);
 	free(ps->a);
-	ps = NULL;
+	ps->a = NULL;
 	ps->size = 0;
 	ps->capacity = 0;
 }
@@ -38,7 +38,7 @@ void SeqCheckCapacity(SeqList* ps)
 	if (ps->size == ps->capacity)
 	{
 		int newcapacity = ps->capacity == 0 ? 4 : 2 * ps->capacity;
-		int newA = realloc(ps->a, newcapacity*sizeof(SLDataType));
+		SLDataType* newA = realloc(ps->a, newcapacity*sizeof(SLDataType));
 		if (newA == NULL)
 		{
 			printf("realloc fail\n");
@@ -52,11 +52,98 @@ void SeqCheckCapacity(SeqList* ps)
 //头插
 void SeqListPushFront(SeqList* ps, SLDataType x)
 {
-	assert(ps);
-	SeqCheckCapacity(ps);
+	//assert(ps);
+	//SeqCheckCapacity(ps);//检查容量
+	//int i = 0;
+	//for (i = ps->size; i > 0; i--)
+	//{
+	//	ps->a[i] = ps->a[i - 1];
+	//}
+	//ps->a[0] = x;
+	//ps->size++;
+
+	SeqListInsert(ps, 0, x);
 }
 
 //尾插
-void SeqListPushBack(SeqList* ps, SLDataType x);
+void SeqListPushBack(SeqList* ps, SLDataType x)
+{
+	//assert(ps);
+	//SeqCheckCapacity(ps);//检查容量
+	//ps->a[ps->size] = x;
+	//ps->size++;
+
+	SeqListInsert(ps, ps->size, x);
+}
+
 //指定下标位置插入
-void SeqListInsert(SeqList* ps, int pos, SLDataType x);
+void SeqListInsert(SeqList* ps, int pos, SLDataType x)
+{
+	assert(ps);
+	assert(pos >= 0 && pos <= ps->size);
+	SeqCheckCapacity(ps);//检查容量
+	int i = 0;
+	for (i = ps->size; i > pos; i--)
+	{
+		ps->a[i] = ps->a[i - 1];
+	}
+	ps->a[pos] = x;
+	ps->size++;
+}
+
+//头删
+void SeqListPopFront(SeqList* ps)
+{
+	//assert(ps);
+	//assert(ps->size > 0);
+	//int i = 0;
+	//for (i = 0; i < ps->size - 1; i++)
+	//{
+	//	ps->a[i] = ps->a[i + 1];
+	//}
+	//ps->size--;
+	SeqListErase(ps, 0);
+}
+
+//尾删
+void SeqListPopBack(SeqList* ps)
+{
+	//assert(ps);
+	//assert(ps->size > 0);
+	//ps->size--;
+	SeqListErase(ps, ps->size - 1);
+}
+//指定下标位置删除
+void SeqListErase(SeqList* ps, int pos)
+{
+	assert(ps);
+	assert(ps->size > 0);
+	assert(pos >= 0 && pos < ps->size);
+	int i = 0;
+	for (i = pos; i < ps->size - 1; i++)
+	{
+		ps->a[i] = ps->a[i + 1];
+	}
+	ps->size--;
+}
+
+//查找元素，若有，返回下标，否则返回-1
+int SeqListFind(SeqList* ps, SLDataType x)
+{
+	assert(ps);
+	int i = 0;
+	for (i = 0; i < ps->size; i++)
+	{
+		if (ps->a[i] == x)
+			return i;
+	}
+	return -1;
+}
+
+//修改指定下标位置元素
+void SeqListModify(SeqList* ps, int pos, SLDataType x)
+{
+	assert(ps);
+	assert(pos >= 0 && pos < ps->size);
+	ps->a[pos] = x;
+}
--- SeqList/SeqList/test.c
+++ SeqList/SeqList/test.c
@@ -4,9 +4,56 @@ void SeqListTest1()
 	SeqList s;
 	SeqListInit(&s);
 
+	SeqListPushBack(&s, 0);
+	SeqListPushBack(&s, 1);
+	SeqListPushBack(&s, 2);
+	SeqListPushBack(&s, 3);
+	SeqListPrint(&s);
+
+	SeqListPushFront(&s, 0);
+	SeqListPushFront(&s, 0);
+	SeqListPushFront(&s, 0);
+	SeqListPrint(&s);
+
+	SeqListInsert(&s, 2, 9);
+	SeqListInsert(&s, 2, 9);
+	SeqListInsert(&s, 2, 9);
+	SeqListPrint(&s);
+
+	SeqListDestory(&s);
+}
+void SeqListTest2()
+{
+	SeqList s;
+	SeqListInit(&s);
+
+	SeqListPushBack(&s, 0);
+	SeqListPushBack(&s, 1);
+	SeqListPushBack(&s, 2);
+	SeqListPushBack(&s, 3);
+	SeqListPrint(&s);
+
+	SeqListPopFront(&s);
+	SeqListPrint(&s);
+
+	SeqListPopBack(&s);
+	SeqListPrint(&s);
+
+	SeqListPushBack(&s, 3);
+	SeqListPushBack(&s, 4);
+	SeqListPushBack(&s, 5);
+	SeqListErase(&s, 2);
+	SeqListPrint(&s);
+
+	int ret = SeqListFind(&s, 2);
+	printf("%d\n", ret);
+	SeqListModify(&s, 2, 9);
+	SeqListPrint(&s);
 }
 int main()
 {
-	SeqListTest1();
+	//SeqListTest1();//≤Â»Î≤‚ ‘
+	SeqListTest2();//…æ≥˝≤‚ ‘
+
 	return 0;
 }
