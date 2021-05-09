7bc38b76922b999b621a20dd3a4364fd740dec59
chenlong_cxy <chenlonghyl@163.com>
2021-05-09 11:25:01 +0800

    希尔排序

--- Sort/Sort/Sort.c
+++ Sort/Sort/Sort.c
@@ -1,16 +1,27 @@
 #include "Sort.h"
 
-//选择排序
+//打印数组
+void PrintArr(int* a, int n)
+{
+	int i = 0;
+	for (i = 0; i < n; i++)
+	{
+		printf("%d ", a[i]);
+	}
+	printf("\n");
+}
+
+//插入排序
 void InsertSort(int* a, int n)
 {
 	int i = 0;
 	for (i = 0; i < n - 1; i++)
 	{
-		int end = n - 1;
-		int tmp = a[i];
+		int end = i;
+		int tmp = a[end + 1];
 		while (end >= 0)
 		{
-			if (a[i] < a[end])
+			if (tmp < a[end])
 			{
 				a[end + 1] = a[end];
 				end--;
@@ -21,5 +32,77 @@ void InsertSort(int* a, int n)
 			}
 		}
 		a[end + 1] = tmp;
+	}
+}
+
+//希尔排序
+void ShellSort(int* a, int n)
+{
+	int gap = n;
+	while (gap > 1)
+	{
+		gap = gap / 2;
+		int i = 0;
+		for (i = 0; i < n - gap; i++)
+		{
+			int end = i;
+			int tmp = a[end + gap];
+			while (end >= 0)
+			{
+				if (tmp < a[end])
+				{
+					a[end + gap] = a[end];
+					end -= gap;
+				}
+				else
+				{
+					break;
+				}
+			}
+			a[end + gap] = tmp;
+		}
+		printf("gap:%d->", gap);
+		PrintArr(a, n);
+	}
+}
+
+//交换函数
+void Swap(int* p1, int* p2)
+{
+	int tmp = *p1;
+	*p1 = *p2;
+	*p2 = tmp;
+}
+
+//选择排序(一次选一个数)
+void SelectSort1(int* a, int n)
+{
+	int i = 0;
+	for (i = 0; i < n; i++)
+	{
+		int start = i;
+		int min = start;
+		while (start < n)
+		{
+			if (a[start] < a[min])
+				min = start;
+			start++;
+		}
+		Swap(&a[i], &a[min]);
+	}
+}
+
+//选择排序(一次选两个数)
+void SelectSort2(int* a, int n)
+{
+	int left;
+	int right;
+	int min = left;
+	int max = left;
+	int i = 0;
+	for (i = left; i < right; i++)
+	{
+		if (a[i] < a[min])
+			min = a[i];
 	}
 }
--- Sort/Sort/Sort.h
+++ Sort/Sort/Sort.h
@@ -1,5 +1,16 @@
 #include <stdio.h>
 
+//打印数组
+void PrintArr(int* a, int n);
 
-//选择排序
+//插入排序
 void InsertSort(int* a, int n);
+
+//希尔排序
+void ShellSort(int* a, int n);
+
+//选择排序(一次选一个数)
+void SelectSort1(int* a, int n);
+
+//选择排序(一次选两个数)
+void SelectSort2(int* a, int n);
