1f094bfd9ba09579e911e6206e7d1f3a2f8dc5eb
chenlong_cxy <chenlonghyl@163.com>
2021-05-09 22:02:32 +0800

    排序

--- Sort/Sort/Sort.c
+++ Sort/Sort/Sort.c
@@ -17,21 +17,24 @@ void InsertSort(int* a, int n)
 	int i = 0;
 	for (i = 0; i < n - 1; i++)
 	{
-		int end = i;
-		int tmp = a[end + 1];
+		int end = i;//记录有序序列的最后一个元素的下标
+		int tmp = a[end + 1];//待插入的元素
 		while (end >= 0)
 		{
-			if (tmp < a[end])
+			if (tmp < a[end])//还需继续比较
 			{
 				a[end + 1] = a[end];
 				end--;
 			}
-			else
+			else//找到应插入的位置
 			{
 				break;
 			}
 		}
 		a[end + 1] = tmp;
+		//代码执行到此位置有两种情况:
+		//1.待插入元素找到应插入位置（break跳出循环到此）。
+		//2.待插入元素比当前有序序列中的所有元素都小（while循环结束后到此）。
 	}
 }
 
